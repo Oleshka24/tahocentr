435a45d399b7979d0ee8e545061607a24a78a041
Oleshka24 <45318220+Oleshka24@users.noreply.github.com>
2020-10-08 18:01:09 +0300

    Adaptation

--- js/main.js
+++ js/main.js
@@ -11,3 +11,34 @@ $('.owl-stage').css("display", "flex");
 $('.owl-stage-outer').css("overflow", "hidden");
 $('.owl-nav.disabled').css("display", "none");
 $('.owl-dots.disabled').css("display", "none");
+
+var maxTopVal = 5;
+
+$(window).on("scroll", function() {
+    if ($(window).scrollTop() > maxTopVal) $('.header').addClass('header--fixed');
+    else if (!$('.header__burger').hasClass("burger--close")) $('.header').removeClass('header--fixed');
+});
+
+$(window).resize(function () {
+    $(window).on("scroll", function() {
+        if ($(window).scrollTop() > maxTopVal) $('.header').addClass('header--fixed');
+        else if (!$('.header__burger').hasClass("burger--close")) $('.header').removeClass('header--fixed');
+    });
+});
+
+$('.burger__btn').click(function(){
+    $(this).parent('.header__burger').toggleClass("burger--close");
+
+    if ($('.header__burger').hasClass("burger--close")) {
+        $('.header').addClass("header--fixed");
+        $('.header__right').addClass("header__right--show");
+    }
+    else {
+        if ($(window).scrollTop() <= maxTopVal) $('.header').removeClass("header--fixed");
+        $('.header__right').removeClass("header__right--show");
+    }
+});
+
+$('.nav__item--dropdown .nav__link').click(function() {
+    $(this).siblings($('.nav__subnav')).toggleClass('nav__subnav--show');
+})
